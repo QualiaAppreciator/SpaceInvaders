27092c065f10a0963b8e5b7660df253d711689cb
Joshua Glyn-Cuthbert <158163991+JoshuaG-C@users.noreply.github.com>
2024-04-24 23:35:44 +0200

    multiplayer update

--- project_main.py
+++ project_main.py
@@ -1,17 +1,15 @@
 import stddraw as s
-import stdaudio
 import functions as f
 import math, time
 from picture import Picture
 from gameObjects import Enemies, Missiles, Player
-from threading import Thread
-
 
 BACKGROUND = Picture("background.PNG")
 ENEMIES = []
 MISSILES = []
 ENEMY_MISSILES = []
 BUNKERS = []
+MISSILES2 = []
 
 
 def main():
@@ -19,13 +17,10 @@ def main():
     overall = True
     PLAYER_GRAPHIC = Picture("player1.PNG")
 
-    #music_thread = Thread(target=f.music,daemon=True)
-    #music_thread.start()
-
     while overall:
         menu = True
         gamePlay = True
-        twoPlayers = True
+        twoPlayers = False
 
         while menu:
             f.drawMenu()
@@ -46,15 +41,13 @@ def main():
 
         f.populateENEMIES(ENEMIES)
         f.populateBUNKERS(BUNKERS)
-
-        player = Player(0,25, math.pi/2, PLAYER_GRAPHIC)
+        player = Player(0,25, math.pi/2, PLAYER_GRAPHIC,1)
         player_last_fired = 0
-
         if twoPlayers:
-            player2 = Player(125,25,math.pi/2,PLAYER_GRAPHIC)
+            player2 = Player(125,25,math.pi/2,PLAYER_GRAPHIC,1)
             player2_last_fired = 0
             player._x = -125
-
+        score = 0
         levelCount = 1
         if gamePlay:
             levelCount = f.levelDisplay(levelCount)
@@ -63,7 +56,23 @@ def main():
             key = s.getKeysPressed()
             s.clear()
             s.picture(BACKGROUND)
-            f.score()
+            f.score(score)
+            gameStatus = f.gameStatus(ENEMIES, ENEMY_MISSILES)
+            player._hitpoints = f.checkIfPlayerHit(player, ENEMY_MISSILES, ENEMIES)
+
+            for k in ENEMIES:
+                k.draw()
+                k.move()
+            for j in MISSILES:
+                j.draw()
+                j.move()
+            for g in ENEMY_MISSILES:
+                g.draw()
+                g.move()
+            for p in BUNKERS:
+                p.draw()
+            player.draw()
+            player.drawCannon()  
 
             if key[s.K_a]:
                 player.move('left')
@@ -73,13 +82,15 @@ def main():
                 player.moveCannon('j')
             if key[s.K_6]:
                 player.moveCannon('l')
-            if key[s.K_SPACE] and (time.time() - player_last_fired > .6):
+            if key[s.K_SPACE] and (time.time() - player_last_fired > .6) and player._hitpoints > 0:
                 MISSILES.append(Missiles(player._x, player._y, player._theta, 0))
-                # pew_thread = Thread(target=f.pew,daemon=True)
-                # pew_thread.start()
                 player_last_fired = time.time()
 
+# everthing below 'if twoPlayers' will run if twoPlayers is true, this will implement multiplayer mode
             if twoPlayers:
+                for i in MISSILES2:
+                    i.draw()
+                    i.move()
                 player2.draw()
                 player2.drawCannon()
                 if key[s.K_j]:
@@ -90,27 +101,42 @@ def main():
                     player2.moveCannon('left')
                 if key[s.K_m]:
                     player2.moveCannon('right')
-                if key[s.K_u] and (time.time() - player2_last_fired > .6):
-                    MISSILES.append(Missiles(player2._x, player2._y, player2._theta, 0))
+                if key[s.K_u] and (time.time() - player2_last_fired > .6) and player2._hitpoints > 0:
+                    MISSILES2.append(Missiles(player2._x, player2._y, player2._theta, 0))
                     player2_last_fired = time.time()
 
-                f.enemyCounterattack(player2._x, ENEMY_MISSILES, ENEMIES)
-            
+                f.enemyCounterattack(player2, ENEMY_MISSILES, ENEMIES)
+                score = f.checkForHits(ENEMIES, BUNKERS, MISSILES2, score)
+
+                if player2._hitpoints > 0:
+                    player2._hitpoints = f.checkIfPlayerHit(player2, ENEMY_MISSILES, ENEMIES)
+
+                if (player._hitpoints <= 0 and player2._hitpoints <= 0) or (gameStatus == 'WON' and levelCount == 4):
+                    gamePlay = False
+                    
+                if gameStatus == 'WON' and levelCount < 4:
+                    levelCount = f.levelDisplay(levelCount)
+                    ENEMIES.clear()
+                    MISSILES.clear()
+                    MISSILES2.clear()
+                    ENEMY_MISSILES.clear()
+                    f.populateENEMIES(ENEMIES)
+                    player = Player(-125,25, math.pi/2, PLAYER_GRAPHIC,1)
+                    player2 = Player(125,25,math.pi/2,PLAYER_GRAPHIC,1)
+
             if key[s.K_q]:
                 overall = False
                 gamePlay = False
             if key[s.K_e]:
                 gamePlay = False
 
-            f.moveEverything(ENEMIES, MISSILES, ENEMY_MISSILES, BUNKERS, player)
-            f.checkForHits(ENEMIES, BUNKERS, MISSILES)
-            if levelCount > 2:
-                f.enemyCounterattack(player._x, ENEMY_MISSILES, ENEMIES)
+            score = f.checkForHits(ENEMIES, BUNKERS, MISSILES, score)
+            #if levelCount > 2:
+            f.enemyCounterattack(player, ENEMY_MISSILES, ENEMIES)
 
-            gameStatus = f.gameStatus(ENEMIES, player, ENEMY_MISSILES)
-            if gameStatus == 'LOST' or (gameStatus == 'WON' and levelCount == 4):
+            if (player._hitpoints <= 0 and twoPlayers == False) or (gameStatus == 'WON' and levelCount == 4):
                 gamePlay = False
-            if gameStatus == 'WON' and levelCount < 4:
+            if gameStatus == 'WON' and levelCount < 4 and twoPlayers == False:
                 levelCount = f.levelDisplay(levelCount)
                 ENEMIES.clear()
                 MISSILES.clear()
@@ -121,9 +147,11 @@ def main():
 
         ENEMIES.clear()
         MISSILES.clear()
+        MISSILES2.clear()
         ENEMY_MISSILES.clear()
         if overall and not key[s.K_e]:
-            f.gameOver(levelCount)
+            f.gameOver(levelCount, score)
+
 
 
 if __name__ == '__main__': main()
